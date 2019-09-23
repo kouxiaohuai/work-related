aefa6f3c6c17b67e309f3301c486062b0fabff04
kouxiaohuai <49260903+kouxiaohuai@users.noreply.github.com>
2019-09-23 14:37:18 +0800

    Update area_coverage_probability.py

--- area_coverage_probability/area_coverage_probability.py
+++ area_coverage_probability/area_coverage_probability.py
@@ -5,18 +5,18 @@ Created on Wed Feb 27 08:25:18 2019
 @author: kWX596514
 """
 
-import scipy.stats
+import scipy
 import math
 
 STD = 8
 PL_SLOPE = 3.57
 n = 10000
-PI = math.pi
-EDGE_P = 0.75
-SFM = STD * scipy.stats.norm.ppf(EDGE_P)
+EDGE_P = 0.86
+SFM = scipy.stats.norm(0, STD).ppf(EDGE_P)
 
 area_p = 0
 for i in range(n):
-    p = scipy.stats.norm(0, STD).cdf(SFM + 10 * PL_SLOPE * math.log10(n/(n-i)))
-    area_p += ((1-i/n)**2 - (1-(i+1)/n)**2) * p
+    p = scipy.stats.norm(0, STD).cdf(SFM + 10*PL_SLOPE*math.log10(n/(n-i)))
+    area_p = area_p + ((1-i/n)**2 - (1-(i+1)/n)**2) * p
+
 print(area_p)
